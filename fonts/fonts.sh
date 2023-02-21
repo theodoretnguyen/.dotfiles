16f7fbd70c101815001cf001a468580aa739ef61
Theodore Nguyen <109197491+theodoretnguyen@users.noreply.github.com>
2023-02-20 22:01:16 -0800

    Add nerd fonts script

---
+++ fonts/fonts.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+brew tap homebrew/cask-fonts
+brew install --cask font-fira-code-nerd-font
+brew install --cask font-iosevka-nerd-font
+brew install --cask font-victor-mono-nerd-font
+brew install --cask font-jetbrains-mono-nerd-font
+brew install --cask font-meslo-lg-nerd-font
+brew install --cask font-hack-nerd-font
