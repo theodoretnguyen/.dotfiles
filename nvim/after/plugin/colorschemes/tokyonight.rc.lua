b4766bfaf9f77e58f00e8046404c01371bfa9625
Theodore Nguyen <109197491+theodoretnguyen@users.noreply.github.com>
2023-02-21 20:36:53 -0800

    Fix colorscheme

--- nvim/after/plugin/colorschemes/tokyonight.rc.lua
+++
@@ -1,9 +0,0 @@
-local found, tokyonight = pcall(require, "tokyonight")
-if (not found) then return end
-
--- [[ Configuration ]]
-tokyonight.setup({
-  style = "moon",
-  transparent = false,
-  lualine_bold = true,
-})
