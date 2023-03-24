bc3c690ae03eb16236c4e33afc13ad7f6160657d
Theodore Nguyen <109197491+theodoretnguyen@users.noreply.github.com>
2023-03-24 16:36:24 -0700

    Remove Skim latex config

--- nvim/.config/nvim/lua/plugins/latex.lua
+++ nvim/.config/nvim/lua/plugins/latex.lua
@@ -18,28 +18,8 @@ return {
       "Overfull \\vbox",
     }
 
-    -- [[ PDF ]]
-
-    -- [[ Skim ]]
-    -- vim.g.vimtex_view_method = "skim"
-    -- vim.g.vimtex_view_skim_activate = 1
-    -- vim.g.vimtex_view_skim_sync = 0
-    -- vim.g.vimtex_view_skim_reading_bar = 1
-
-    -- [[ Sioyek ]]
+    -- [[ PDF: Sioyek ]]
     vim.g.vimtex_view_method = "sioyek"
-
-    -- Return focus to Nvim after inverse search on macOS
-    -- vim.cmd([[
-    -- function! s:TexFocusVim() abort
-    -- silent execute "!open -a kitty"
-    -- redraw!
-    -- endfunction
-    -- augroup vimtex_event_focus
-    -- au!
-    -- au User VimtexEventViewReverse call s:TexFocusVim()
-    -- augroup END
-    -- ]])
 
     vim.g.maplocalleader = "\\"
     vim.g.vimtex_mappings_enabled = 1
