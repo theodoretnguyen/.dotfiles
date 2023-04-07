6d0eea03e747d5d85952489e2638cf3fb8c5b6f8
Theodore Nguyen <109197491+theodoretnguyen@users.noreply.github.com>
2023-04-07 10:54:12 -0700

    Change cpp files to tabstop of 2

--- nvim/.config/nvim/lua/theo/autocmd.lua
+++ nvim/.config/nvim/lua/theo/autocmd.lua
@@ -60,9 +60,9 @@ autocmd('FileType', {
 autocmd('FileType', {
   pattern = { 'cpp' },
   callback = function()
-    vim.bo.tabstop = 4
-    vim.bo.softtabstop = 4
-    vim.bo.shiftwidth = 4
+    vim.bo.tabstop = 2
+    vim.bo.softtabstop = 2
+    vim.bo.shiftwidth = 2
     vim.bo.expandtab = true
     vim.bo.autoindent = true
     vim.bo.smartindent = true
