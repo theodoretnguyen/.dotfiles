2a9b97351b2d2b8abc7f88063250511c924fe187
Theodore Nguyen <109197491+theodoretnguyen@users.noreply.github.com>
2023-04-15 23:49:58 -0700

    Reorganize autocompletion plugins

--- nvim/.config/nvim/lua/plugins/autocompletion.lua
+++ nvim/.config/nvim/lua/plugins/autocompletion.lua
@@ -1,51 +1,53 @@
 return {
-  "hrsh7th/nvim-cmp",
-  event = { "InsertEnter" },
-  dependencies = {
-    -- sources
-    "hrsh7th/cmp-nvim-lsp", -- nvim_lsp : LSP source
-    "saadparwaiz1/cmp_luasnip", -- luasnip : snippets source
-    -- kind icons
-    "onsails/lspkind.nvim",
-  },
-  config = function()
-    local cmp = require("cmp")
-    local luasnip = require("luasnip")
+  {
+    "hrsh7th/nvim-cmp",
+    event = { "InsertEnter" },
+    dependencies = {
+      -- sources
+      "hrsh7th/cmp-nvim-lsp", -- nvim_lsp : LSP source
+      "saadparwaiz1/cmp_luasnip", -- luasnip : snippets source
+      -- kind icons
+      "onsails/lspkind.nvim",
+    },
+    config = function()
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
 
-    -- [[ General Configuration ]]
-    cmp.setup({
-      snippet = {
-        expand = function(args)
-          luasnip.lsp_expand(args.body)
-        end,
-      },
-      window = {
-        completion = cmp.config.window.bordered(),
-        documentation = cmp.config.window.bordered(),
-      },
-      mapping = cmp.mapping.preset.insert({
-        ["<C-Space>"] = cmp.mapping.complete(),
-        ["<C-n>"] = cmp.mapping.select_next_item(),
-        ["<C-p>"] = cmp.mapping.select_prev_item(),
-        ["<C-u>"] = cmp.mapping.scroll_docs(-4),
-        ["<C-d>"] = cmp.mapping.scroll_docs(4),
-        ["<C-e>"] = cmp.mapping.abort(),
-        ["<CR>"] = cmp.mapping.confirm({ select = true }),
-      }),
-      formatting = {
-        format = require('lspkind').cmp_format({
-          mode = "symbol_text",
-          preset = "default",
-          menu = ({
-            nvim_lsp = "[LSP]",
-            luasnip = "[LuaSnip]",
-          }),
+      -- [[ General Configuration ]]
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        window = {
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-n>"] = cmp.mapping.select_next_item(),
+          ["<C-p>"] = cmp.mapping.select_prev_item(),
+          ["<C-u>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-d>"] = cmp.mapping.scroll_docs(4),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
         }),
-      },
-      sources = {
-        { name = "nvim_lsp" },
-        { name = "luasnip" },
-      },
-    })
-  end,
+        formatting = {
+          format = require('lspkind').cmp_format({
+            mode = "symbol_text",
+            preset = "default",
+            menu = ({
+              nvim_lsp = "[LSP]",
+              luasnip = "[LuaSnip]",
+            }),
+          }),
+        },
+        sources = {
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+        },
+      })
+    end,
+  },
 }
