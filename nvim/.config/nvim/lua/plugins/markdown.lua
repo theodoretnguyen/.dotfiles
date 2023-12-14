8646e7f67e7eeb59577707f74591d4995e51b326
Theodore Nguyen <109197491+theodoretnguyen@users.noreply.github.com>
2023-12-14 09:58:55 -0800

    Update build function

--- nvim/.config/nvim/lua/plugins/markdown.lua
+++ nvim/.config/nvim/lua/plugins/markdown.lua
@@ -1,7 +1,9 @@
 return {
   {
     "iamcco/markdown-preview.nvim",
-    build = "cd app && npm install",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = function() vim.fn["mkdp#util#install"]() end,
+    enabled = true,
     init = function()
       vim.g.mkdp_filetypes = { "markdown" }
     end,
